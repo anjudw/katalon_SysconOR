eba892d0c7d5514a787353a642192006ea7ea1a6
anjudw <81610429+anjudw@users.noreply.github.com>
2023-08-03 10:46:36 +0530

    Add files via upload

---
+++ ToastMessage/SuccessMessage.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>SuccessMessage</name>
+   <tag></tag>
+   <elementGuidId>34b13718-5591-4c25-a07c-dc2446856b96</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='toastHeading']/h3[contains(text(), &quot;Success!&quot;)]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='toastHeading']/h3[contains(text(), &quot;Success!&quot;)]</value>
+      <webElementGuid>9f60384c-0a55-4670-b9e8-9980c0c79fa2</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ ToastMessage/WarningToast.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>WarningToast</name>
+   <tag></tag>
+   <elementGuidId>8784d494-cf8c-4dd2-b660-1f383a0b174d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='toastHeading']/h3[contains(text(), &quot;Warning!&quot;)]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='toastHeading']/h3[contains(text(), &quot;Warning!&quot;)]</value>
+      <webElementGuid>617310ef-b4df-47df-bf71-3fa6689c999d</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
